f3d0fad3decdc2c42460c3c37f4dbe1f974a7238
EmmanuelOmale <eomale@gmail.com>
2023-08-06 00:51:58 +0100

    GuardClauses-Implementation-For-CatalogItem-Completed

--- ApplicationCore/CatalogItem.cs
+++ ApplicationCore/CatalogItem.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ardalis.GuardClauses;
 
 namespace ApplicationCore
 {
     public  class CatalogItem : BaseEntity
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }  
-        public string PictureUri { get; set; }  
-        public int CatalogTypeId { get; set; }
-        public CatalogType? CatalogType { get; set; }
-        public int CatalogBrandId { get; set; }
-        public CatalogBrand? CatalogBrand { get; set; }
-
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }  
+        public string PictureUri { get; private set; }  
+        public int CatalogTypeId { get; private set; }
+        public CatalogType? CatalogType { get; private set; }
+        public int CatalogBrandId { get; private set; }
+        public CatalogBrand? CatalogBrand { get; private set; }
 
         public CatalogItem(int catalogTypeId,
             int catalogBrand,
@@ -31,6 +26,53 @@ namespace ApplicationCore
             Name = name;
             Price = price;
             PictureUri = pictureUri;
+        }
+
+        public void UpdateDetails(CatalogItemDetails details)
+        {
+            Guard.Against.NullOrEmpty(details.Name, nameof(details.Name));
+            Guard.Against.NullOrEmpty(details.Description, nameof(details.Description));
+            Guard.Against.NegativeOrZero(details.Price, nameof(details.Price));
+
+            Name = details.Name;
+            Description = details.Description;
+            Price = details.Price;
+        }
+
+        public void UpdateBrand(int catalogBrandId)
+        {
+            Guard.Against.Zero(catalogBrandId, nameof(catalogBrandId));
+            CatalogBrandId = catalogBrandId;
+        }
+
+        public void UpdateType(int catalogTypeId )
+        {
+            Guard.Against.Zero(catalogTypeId, nameof(catalogTypeId));
+            CatalogTypeId = catalogTypeId;
+        }
+
+        public void UpdatePictureUri(string  pictureName)
+        {
+            if(string.IsNullOrEmpty(pictureName))
+            {
+                PictureUri = string.Empty;
+                return;
+            }
+            PictureUri = $"images\\products\\{pictureName}?{new DateTime().Ticks}";
+        }
+
+        public readonly record struct CatalogItemDetails
+        {
+            public string? Name { get; }
+            public string? Description { get; } 
+            public decimal Price { get; }
+
+            public CatalogItemDetails(string? name, string? description, decimal price)
+            {
+                Name = name;
+                Description = description;
+                Price = price;
+            }
         }
     }
 }
