3ebb40fa0e171d881711f8340a152a6aed5cb21c
EmmanuelOmale <eomale@gmail.com>
2023-08-06 11:02:16 +0100

    Shopping-cart-ready

---
+++ ApplicationCore/CartAggregate/Cart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.CartAggregate
+{
+    public class Cart : BaseEntity
+    {
+        public string BuyerId { get; private set; }
+        private readonly List<CartItem> _items = new List<CartItem>();
+        public IReadOnlyCollection<CartItem> Items => _items.AsReadOnly();
+        public int TotalItems => _items.Sum(i => i.Quantity);
+
+        public Cart(string buyerId)
+        {
+            BuyerId = buyerId;
+        }
+
+        public void AddItem(int catalogItemId, decimal unitPrice, int quantity = 1)
+        {
+            if(!Items.Any(i => i.CatalogItemId == catalogItemId))
+            {
+                _items.Add(new CartItem(catalogItemId, quantity, unitPrice));
+                return;
+            }
+            var existingItems = Items.First(i => i.CatalogItemId == catalogItemId);
+            existingItems.AddQuantity(quantity);
+        }
+
+        public void RemoveEmptyItems()
+        {
+            _items.RemoveAll(i => i.Quantity == 0);
+        }
+
+        public void SetNewBuyerId(string buyerId)
+        {
+            BuyerId = buyerId;
+        }
+    }
+    
+}
---
+++ ApplicationCore/CartAggregate/CartItem.cs
@@ -0,0 +1,36 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.CartAggregate
+{
+    public class CartItem : BaseEntity
+    {
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }   
+        public int CatalogItemId { get; set; }
+        public int CartId { get; set; }
+
+        public CartItem(int catalogItemId, int quantity, decimal unitPrice)
+        {
+            CatalogItemId = catalogItemId;
+            UnitPrice = unitPrice;
+            SetQuantity(quantity);
+        }
+
+        public void AddQuantity(int quantity)
+        {
+            Guard.Against.OutOfRange(quantity, nameof(quantity), 0, int.MaxValue);
+            Quantity += quantity;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            Guard.Against.OutOfRange(quantity, nameof(quantity), 0, int.MaxValue);
+            Quantity += quantity;
+        }
+    }
+}
